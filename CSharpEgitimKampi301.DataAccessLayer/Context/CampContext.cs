b19af4086b7860040769349d4401cb841b10e8c6
Aysegul Yildiz <ayildiz.2212@gmail.com>
2025-08-29 20:01:26 +0300

    Interfaces eklendi

--- CSharpEgitimKampi301.DataAccessLayer/Context/CampContext.cs
+++ CSharpEgitimKampi301.DataAccessLayer/Context/CampContext.cs
@@ -14,7 +14,6 @@ namespace CSharpEgitimKampi301.DataAccessLayer.Context
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
-
         public DbSet<Admin> Admins { get; set; }
 
         // Yalın hali sınıf adı C# tarafında kullanılır,
