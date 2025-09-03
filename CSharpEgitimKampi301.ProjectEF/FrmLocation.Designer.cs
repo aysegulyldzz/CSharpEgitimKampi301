d5853d7158faf4beff72a8c36cfd6aacc82499fa
Aysegul Yildiz <ayildiz.2212@gmail.com>
2025-09-04 02:39:34 +0300

    FrmLocation eklendi, butonlardan devam edilecek

---
+++ CSharpEgitimKampi301.ProjectEF/FrmLocation.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace CSharpEgitimKampi301.ProjectEF
+{
+    partial class FrmLocation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textCountry = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textLocId = new System.Windows.Forms.TextBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textDayNight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textPrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboGuide = new System.Windows.Forms.ComboBox();
+            this.nudCapacity = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCapacity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.BackColor = System.Drawing.Color.Turquoise;
+            this.btnGetById.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGetById.Location = new System.Drawing.Point(30, 372);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(306, 45);
+            this.btnGetById.TabIndex = 23;
+            this.btnGetById.Text = "Id\'ye Göre Getir";
+            this.btnGetById.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Turquoise;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdate.Location = new System.Drawing.Point(189, 305);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(147, 45);
+            this.btnUpdate.TabIndex = 22;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.Turquoise;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnRemove.Location = new System.Drawing.Point(30, 305);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(147, 45);
+            this.btnRemove.TabIndex = 21;
+            this.btnRemove.Text = "Sil";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Turquoise;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.Location = new System.Drawing.Point(189, 243);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(147, 45);
+            this.btnAdd.TabIndex = 20;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // textCountry
+            // 
+            this.textCountry.Location = new System.Drawing.Point(134, 75);
+            this.textCountry.Name = "textCountry";
+            this.textCountry.Size = new System.Drawing.Size(202, 22);
+            this.textCountry.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Ülke Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Kapasite:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(365, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(409, 372);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // textLocId
+            // 
+            this.textLocId.Location = new System.Drawing.Point(134, 45);
+            this.textLocId.Name = "textLocId";
+            this.textLocId.Size = new System.Drawing.Size(202, 22);
+            this.textLocId.TabIndex = 14;
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.Color.Turquoise;
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnList.Location = new System.Drawing.Point(30, 243);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(147, 45);
+            this.btnList.TabIndex = 13;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Lokasyon Id:";
+            // 
+            // textDayNight
+            // 
+            this.textDayNight.Location = new System.Drawing.Point(134, 165);
+            this.textDayNight.Name = "textDayNight";
+            this.textDayNight.Size = new System.Drawing.Size(202, 22);
+            this.textDayNight.TabIndex = 29;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Gün - Gece:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Rehber Id:";
+            // 
+            // textPrice
+            // 
+            this.textPrice.Location = new System.Drawing.Point(134, 135);
+            this.textPrice.Name = "textPrice";
+            this.textPrice.Size = new System.Drawing.Size(202, 22);
+            this.textPrice.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 16);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Toplam Fiyat:";
+            // 
+            // comboGuide
+            // 
+            this.comboGuide.FormattingEnabled = true;
+            this.comboGuide.Location = new System.Drawing.Point(134, 195);
+            this.comboGuide.Name = "comboGuide";
+            this.comboGuide.Size = new System.Drawing.Size(202, 24);
+            this.comboGuide.TabIndex = 30;
+            // 
+            // nudCapacity
+            // 
+            this.nudCapacity.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudCapacity.Location = new System.Drawing.Point(134, 105);
+            this.nudCapacity.Name = "nudCapacity";
+            this.nudCapacity.Size = new System.Drawing.Size(202, 22);
+            this.nudCapacity.TabIndex = 31;
+            this.nudCapacity.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // FrmLocation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 465);
+            this.Controls.Add(this.nudCapacity);
+            this.Controls.Add(this.comboGuide);
+            this.Controls.Add(this.textDayNight);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textPrice);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textCountry);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textLocId);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmLocation";
+            this.Text = "FrmLocation";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCapacity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetById;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textCountry;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textLocId;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textDayNight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboGuide;
+        private System.Windows.Forms.NumericUpDown nudCapacity;
+    }
+}
---
+++ CSharpEgitimKampi301.ProjectEF/FrmLocation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.ProjectEF
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+
+        EgitimKampiEFTravelDbEntities1 db = new EgitimKampiEFTravelDbEntities1();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.TblLocation
+                .Select(x => new
+                {
+                    x.LocationId,
+                    x.Country,
+                    x.Capacity,
+                    x.Price,
+                    x.DayNight,
+                    GuideId = x.GuideId,
+                    GuideName = x.TblGuide.GuideName,
+                    GuideSurname = x.TblGuide.GuideSurname
+                })
+                .ToList();
+
+            dataGridView1.DataSource = values;
+        }
+    }
+}
